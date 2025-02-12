a9d88df11db93799dffb64e23ef85278dd7d0b4c
Jorge Coutinho <juniorjrcsccp@gmail.com>
2025-02-11 23:33:52 -0300

    [#1] - Creates project and configs

---
+++ src/main/java/com/juniorjrc/orderapigateway/OrderApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.juniorjrc.orderapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApiGatewayApplication.class, args);
+	}
+
+}
